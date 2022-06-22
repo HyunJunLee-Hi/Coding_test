9d36e74d5e8323b526088ab53b2a6185322080aa
HyunJunLee-Hi <nejukja@gmail.com>
2022-06-22 22:45:13 +0900

    second commit

--- 22.06.22_백준.py
+++ 22.06.22_백준.py
@@ -28,3 +28,67 @@ for i in range(t):
     print("Case #" + str(i+1) + ": ", end='')
     print(answer)
 
+
+
+#5430 AC
+#Reverse 시간 줄이는 팁 : Reverse count 후 마지막에만(삭제는 pop과 popleft 혼용)
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+t = int(input())
+for i in range(t):
+    func = input().rstrip()
+    func = list(func)
+    n = int(input())
+    lst = input().rstrip()
+    lst = lst[1:]
+    q = deque()
+    temp = ""
+    for j in lst:
+        if j == ',' or j == ']':
+            if temp != "":
+                q.append(int(temp))
+                temp = ""
+            else:
+                break
+            continue
+        else:
+            temp += j
+    R_flag = 0
+    for j in func:
+        flag = 0
+        if j == 'R':
+            # temp = deque()
+            # for i in range(n):
+            #     temp.appendleft(q[i])
+            # q = temp
+            R_flag += 1
+        elif j == 'D':
+            if q:
+                if R_flag % 2 == 0:
+                    q.popleft()
+                else:
+                    q.pop()
+            else:
+                print("error")
+                flag = 1
+                break
+    if R_flag % 2 == 1:
+        temp = deque()
+        for i in range(len(q)):
+            temp.appendleft(q[i])
+        q = temp
+    if flag == 0:
+        res = "["
+        n = len(q)
+        cnt = 0
+        while n-1 > cnt:
+            res += str(q.popleft())
+            res += ","
+            cnt += 1
+        if q:
+            res += str(q.popleft())
+        res += "]"
+        print(res)
+
