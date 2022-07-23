fcb541e764596816a733527aae133ba2c6e60936
HyunJunLee-Hi <nejukja@gmail.com>
2022-07-23 23:30:13 +0900

    second commit

--- 22.07.23_프로그래머스.py
+++ 22.07.23_프로그래머스.py
@@ -9,3 +9,11 @@ def solution(n):
             answer = i
             break
     return answer
+
+#땅따먹기
+def solution(land):
+    answer = 0
+    for i in range(1, len(land)):
+        for j in range(4):
+            land[i][j] += max(land[i-1][k] for k in range(4) if j != k)
+    return(max(land[-1]))
